c49259d139794cafe7bde92eaeb81c9b342d5099
kenn_ngena <ngenakenn@yahoo.com>
2023-09-05 17:51:40 +0200

    debugage

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ app.use(cors())
 const url = "mysql://bb085ce4cf2349:ee6f33f0@eu-cdbr-west-03.cleardb.net/heroku_ec93d0f43bc3ec0?reconnect=true"
 
 const db = mysql.createConnection({
-	host:"eu-cdbr-west-03.cleardb.net",
-	user:"bb085ce4cf2349",
-	//port:process.env.MYSQLPORT,
-	password: "ee6f33f0",
-	database:"heroku_ec93d0f43bc3ec0",
+	host:"localhost",
+	user:"root",
+	//port:3306,
+	password:"" ,
+	database:"reservationlist",
 }
 );
 app.post('/reservationlist',async (req,res) => {
-	const sql = "INSERT INTO reservationlist.liste (`name`,`email`,`date`,`price`,`article`) VALUES(?)"
+	const sql = "INSERT INTO liste (`name`,`email`,`date`,`price`,`article`) VALUES(?)"
 	const values = [
 		req.body.name,
 		req.body.email,
@@ -42,11 +42,25 @@ app.post('/reservationlist',async (req,res) => {
 	})
 })
 app.post('/reservationuser',async (req,res) => {
-	const sql = "SELECT * FROM reservationlist.liste WHERE  email = ? ";
+	const sql = "SELECT * FROM liste WHERE  email = ? ";
 	const values = [
 		req.body.email,
 	]
 	await db.query(sql, [values],(err, data) =>{
+		if(err){
+			console.log(err)
+		}
+		if(data.length > 0){
+			return res.json(data);
+		} else {
+			return res.json([]);
+		}
+	})
+})
+app.post('/listesale',async (req,res) => {
+	const sql = "SELECT * FROM liste ";
+	
+	await db.query(sql ,(err, data) =>{
 		if(err){
 			console.log(err)
 		}
